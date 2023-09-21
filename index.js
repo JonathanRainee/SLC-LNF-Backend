2189773bbf9898c93817606ce932ba2c564c4a0a
Jonathan Raine <jonatannnraineeee@gmail.com>
2023-09-21 12:56:54 +0700

    migrate db
    migrate db

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express")
 const app = express()
+const {PrismaClient} = require("@prisma/client")
+
+const prisma = new PrismaClient()
 
 app.use(express.json())
+
+app.get("/", async(req, res)=>{
+    const allItem = await prisma.item.findMany()
+    res.json(allItem)
+})
 
 app.listen(3001, ()=> console.log(`server running on port ${3001}`))
